52aac55abb907f405f9eca03d521d0e98c34276c
Thomas Dickhut <thomas.dickhut@gfi.ihk.de>
2025-11-18 15:33:52 +0100

    Issue #122: Umstellung auf das neue Material-Theme 3 inklusive Design Tokens

--- projects/lux-components-lib/src/lib/lux-popups/lux-snackbar/lux-snackbar-component/lux-snackbar.component.ts
+++ projects/lux-components-lib/src/lib/lux-popups/lux-snackbar/lux-snackbar-component/lux-snackbar.component.ts
@@ -5,7 +5,6 @@ import { MAT_SNACK_BAR_DATA, MatSnackBarRef } from '@angular/material/snack-bar'
 import { Observable, Subject } from 'rxjs';
 import { LuxIconComponent } from '../../../lux-icon/lux-icon/lux-icon.component';
 import { LuxSnackbarColor, LuxSnackbarColors } from '../../../lux-util/lux-colors.enum';
-import { LuxUtil } from '../../../lux-util/lux-util';
 import { LuxSnackbarConfig } from '../lux-snackbar-config';
 
 @Component({
@@ -19,20 +18,12 @@ export class LuxSnackbarComponent implements OnInit {
 
   private action$: Subject<void> = new Subject<void>();
 
-  textFontColor = '';
-  actionFontColor = '';
-  iconFontColor = '';
-
   ngOnInit() {
     Object.keys(this.config).forEach((key: string) => {
       if ((this.config as any)[key]) {
         (this.config as any)[key] = (this.config as any)[key].trim();
       }
     });
-    // stupid-cast, um den string weiterzugeben, da die fn daraus den enum-wert herleiten kann
-    this.textFontColor = LuxUtil.getColorsByBgColorsEnum(this.checkColorInEnum(this.config.textColor)).backgroundCSSClass;
-    this.actionFontColor = LuxUtil.getColorsByBgColorsEnum(this.checkColorInEnum(this.config.actionColor)).backgroundCSSClass;
-    this.iconFontColor = LuxUtil.getColorsByBgColorsEnum(this.checkColorInEnum(this.config.iconColor)).backgroundCSSClass;
   }
 
   onAction(): Observable<void> {
