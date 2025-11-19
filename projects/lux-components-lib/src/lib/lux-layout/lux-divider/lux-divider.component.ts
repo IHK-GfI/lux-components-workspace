8cb9c09019f42817bcb1c88077bc6aab1c41502e
Thomas Dickhut <thomas.dickhut@gfi.ihk.de>
2025-11-19 08:18:44 +0100

    Issue #122: Umstellung auf das neue Material-Theme 3 inklusive Design Tokens

--- projects/lux-components-lib/src/lib/lux-layout/lux-divider/lux-divider.component.ts
+++ projects/lux-components-lib/src/lib/lux-layout/lux-divider/lux-divider.component.ts
@@ -1,35 +1,20 @@
-import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'lux-divider',
   templateUrl: './lux-divider.component.html',
   styleUrls: ['./lux-divider.component.scss'],
-  imports: [MatDivider]
+  imports: [MatDivider],
+  host: {
+    '[class.lux-vertical-divider]': 'luxVertical()',
+    '[class.lux-horizontal-divider]': '!luxVertical()'
+  }
+  ,changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LuxDividerComponent implements OnInit {
-  private _luxVertical = false;
-
-  @Input() luxInset = false;
-
-  @HostBinding('class') classes = '';
-
-  get luxVertical(): boolean {
-    return this._luxVertical;
-  }
-
-  @Input() set luxVertical(vertical: boolean) {
-    this._luxVertical = vertical;
-    this.updateHostClasses();
-  }
+export class LuxDividerComponent {
+  luxInset = input<boolean>(false);
+  luxVertical = input<boolean>(false);
 
   constructor() {}
-
-  ngOnInit() {
-    this.updateHostClasses();
-  }
-
-  private updateHostClasses() {
-    this.classes = this.luxVertical ? 'lux-vertical-divider' : 'lux-horizontal-divider';
-  }
 }
