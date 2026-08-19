5fef8e8c5398251446d1ecc64fd085b3e1dba063
Thomas Dickhut <32597554+thodic77@users.noreply.github.com>
2026-08-19 10:35:30 +0200

    Issue #282: Step-Titel wird nicht angezeigt, wenn Header/Content in separate Komponente ausgelagert werden (#283)

---
+++ projects/demo-app/src/app/components-overview/stepper-example/stepper-external-step-example.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { LuxStepComponent } from '@ihk-gfi/lux-components';
+
+@Component({
+  selector: 'app-stepper-external-step-example',
+  styles: [
+    `
+      .step-header {
+        margin-left: 12px;
+      }
+    `
+  ],
+  template: `
+    <ng-template #header><span class="step-header">Person</span></ng-template>
+
+    <ng-template #content>
+      <h3>Ausgelagerter Step</h3>
+      <p>Dieser Step wird als eigene Komponente eingebunden und liefert Header und Content über #header/#content an den Stepper.</p>
+    </ng-template>
+  `,
+  providers: [{ provide: LuxStepComponent, useExisting: StepperExternalStepExampleComponent }]
+})
+export class StepperExternalStepExampleComponent extends LuxStepComponent {}
